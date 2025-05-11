d972b83c821007b9ab52e0c6c94596d99da6bc11
isha220815 <ishanigam1904@gmail.com>
2025-05-11 17:04:17 +0530

    new

--- BajajBackend/src/main/java/com/api/config/StartConfig.java
+++ BajajBackend/src/main/java/com/api/config/StartConfig.java
@@ -17,25 +17,21 @@ public class StartConfig implements CommandLineRunner {
     public void run(String... args) throws Exception {
         String url = "https://bfhldevapigw.healthrx.co.in/hiring/generateWebhook/JAVA";
 
-        // Set headers
+
         HttpHeaders headers = new HttpHeaders();
         headers.setContentType(MediaType.APPLICATION_JSON);
 
-        // Prepare request body
         Map<String, String> requestBody = Map.of(
                 "name", "John Doe",
                 "regNo", "REG12347",
                 "email", "john@example.com"
         );
 
-        // Create HTTP entity with body and headers
         HttpEntity<Map<String, String>> entity = new HttpEntity<>(requestBody, headers);
 
-        // Send POST request using RestTemplate
         RestTemplate restTemplate = new RestTemplate();
         ResponseEntity<String> response = restTemplate.postForEntity(url, entity, String.class);
 
-        // Print the response body
         System.out.println("Webhook Response:\n" + response.getBody());
     }
 }
